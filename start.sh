472bfd3cb1a224f413d1adb1e126daf8f4a49c0e
Alain Lehmann <alain.lehmann@gmail.com>
2022-01-16 19:04:30 +0100

    add creation of disk image

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+DISK_IMG=./disk.img
 args=(
   -m 16G -smp 4
   # accelleration
@@ -8,5 +9,27 @@ args=(
   -netdev user,id=net0,hostfwd=tcp::5555-:22
   # boot device
   -cdrom $NIXOS_ISO
+  # main disk
+  -hda $DISK_IMG
 )
-qemu-system-x86_64 "${args[@]}"
+
+function start() {
+  qemu-system-x86_64 "${args[@]}"
+}
+
+function mkDiskImg() {
+  [ -e $DISK_IMG ] && \
+    echo "Disk image exists. Manually delete $DISK_IMG" && \
+    exit 1
+  qemu-img create -f qcow2 $DISK_IMG 100G
+}
+
+COMMAND=$1
+case "$COMMAND" in
+  --mkimg)
+    mkDiskImg
+    ;;
+  --start)
+    start
+    ;;
+esac
