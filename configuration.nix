7eb88e2d2317a06e0c1c4df76e6c4a0f11af16a8
Alain Lehmann <alain.lehmann@gmail.com>
2022-02-04 00:07:56 +0100

    install a minimal x11 environment

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,8 @@
   # };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
+  services.xserver.windowManager.awesome.enable = true;
 
 
   
