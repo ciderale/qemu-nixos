95fcdedc682b2d68c17ca159ce4f687d709cdf6c
Alain Lehmann <alain.lehmann@gmail.com>
2022-02-03 22:46:53 +0100

    use a stable nixos iso image url

--- flake.nix
+++ flake.nix
@@ -24,8 +24,8 @@
     '';
     OVMF=nixpkgs.legacyPackages.x86_64-linux.OVMFFull.fd;
     NIXOS_ISO = pkgs.fetchurl {
-      url = "https://channels.nixos.org/nixos-21.11/latest-nixos-minimal-x86_64-linux.iso";
-      sha256 = "GMZK+F37p3/i9MxZCYlEu0gTx4qdtblGN2uSnRHjKwE=";
+      url = "https://hydra.nixos.org/build/166431748/download/1/nixos-minimal-21.11.335749.4da27abaebe-x86_64-linux.iso";
+      sha256 = "zdCAmGxy6MegeYa9aPMvLiQcpk0GTfOxyLPcld0wh9I=";
     };
   in rec {
     devShell = pkgs.mkShell {
