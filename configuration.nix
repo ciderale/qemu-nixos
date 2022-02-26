ec398bfa4e6f6124614df491b594da3f3a3d94df
Alain Lehmann <alain.lehmann@gmail.com>
2022-02-26 03:32:44 +0100

    cleanup configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -82,9 +82,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #   wget
+    # wget
     firefox
-    alsa-utils
+    # alsa-utils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
