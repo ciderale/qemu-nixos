2b4cedaf4f93e59953cfd65f7925f398ded3aad9
Alain Lehmann <alain.lehmann@gmail.com>
2022-03-06 01:26:39 +0100

    fix installer hickup

--- vm-install.sh
+++ vm-install.sh
@@ -1,5 +1,12 @@
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 nix-channel --update
 nix-env -iA nixos.nix nixos.git
-mkdir -p /Users && mount -t 9p host_users /Users
-nixos-install --no-root-passwd --flake /Users/ale/Projects/Dev/qemu-nixos#docker-duck
+
+# mount flake repository into vm
+OPTIONS="trans=virtio,version=9p2000.L,msize=104857600"
+mkdir -p /Users && mount -t 9p -o $OPTIONS host_users /Users
+# workaround missing tmp folder (https://github.com/NixOS/nixpkgs/issues/73404)
+mkdir /mnt/mnt && mount --bind /mnt /mnt/mnt
+
+FLAKE=/Users/ale/Projects/Dev/qemu-nixos#docker-duck
+nixos-install --root /mnt --no-root-passwd --flake "$FLAKE"
