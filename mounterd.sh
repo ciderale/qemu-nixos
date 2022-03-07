542bfc2bfcb8e9b82a947ce41c507d628eebf7a3
Alain Lehmann <alain.lehmann@gmail.com>
2022-03-08 00:23:04 +0100

    improve mounterd regarding file/dir changes

--- mounterd.sh
+++ mounterd.sh
@@ -1,16 +1,44 @@
 function mount_sync() {
-  # create bind mounts
-  for i in $(comm -1 -3 <(ls -1 /tmp) <(ls /.tmp/)); do
-    test -d /.tmp/$i && mkdir /tmp/$i || touch /tmp/$i;
-    mount --bind /.tmp/$i /tmp/$i;
-    echo "bind mount $i";
-  done
+  echo "# cleanup stale files/folders"
 
-  # remove stale bind mounts
-  for i in /tmp/*; do
-    test ! -e $i && echo "unbind $i" && umount $i && rm -d $i
+  IFS=$'\n\t'
+  for HOST_TMP in /.tmp/*; do
+    if [ ! \( -f "$HOST_TMP" -o -d "$HOST_TMP" \) ]; then
+      echo "skipping $HOST_TMP"
+      continue
+    fi
+    VM_TMP=${HOST_TMP/.tmp/tmp}
+
+    if [ -f "$VM_TMP" ]; then
+      # triggers potential network read issue in 9p
+      dd if=$VM_TMP count=1 bs=1 >/dev/null 2>/dev/null
+      if [ $? != 0 ]; then
+        echo "remove stale file"
+        umount $VM_TMP
+        rm -d $VM_TMP
+      fi
+    fi
+
+    if [ -d "$VM_TMP" ]; then
+      # triggers potential network read issue in 9p
+      ls $VM_TMP >/dev/null 2>/dev/null
+      if [ $? != 0 ]; then
+        echo "remove stale folder"
+        umount $VM_TMP
+        rm -d $VM_TMP
+      fi
+    fi
+
+    if [ ! -e "$VM_TMP" ]; then
+      # create mount point
+      test -d $HOST_TMP && mkdir $VM_TMP || touch $VM_TMP
+      # bind mount
+      mount --bind $HOST_TMP $VM_TMP
+      echo "bind mount $i";
+    fi
+
   done
 }
 
 echo "starting listening to docker events"
-docker events | while read; do mount_sync; done
+(echo;docker events) | while read; do mount_sync; done
