019bc1ad17920389be6b15f7bda9b5e961a19a88
Alain Lehmann <alain.lehmann@gmail.com>
2022-03-13 23:03:13 +0100

    fix and improve temp file mounter

--- mounterd.sh
+++ mounterd.sh
@@ -1,19 +1,21 @@
 function mount_sync() {
-  echo "# cleanup stale files/folders"
+  echo "# syncing temp files folder from host and vm"
 
   IFS=$'\n\t'
-  for HOST_TMP in /.tmp/*; do
+  # don't mount files owned by root (assuming system files)
+  for HOST_TMP in $(find /.tmp/ ! -user root -maxdepth 1); do
+    #echo "  - $HOST_TMP"
     if [ ! \( -f "$HOST_TMP" -o -d "$HOST_TMP" \) ]; then
       echo "skipping $HOST_TMP"
       continue
     fi
-    VM_TMP=${HOST_TMP/.tmp/tmp}
 
+    VM_TMP=${HOST_TMP/.tmp/tmp}
     if [ -f "$VM_TMP" ]; then
       # triggers potential network read issue in 9p
       dd if=$VM_TMP count=1 bs=1 >/dev/null 2>/dev/null
       if [ $? != 0 ]; then
-        echo "remove stale file"
+        echo "remove stale file [$VM_TMP]"
         umount $VM_TMP
         rm -d $VM_TMP
       fi
@@ -23,7 +25,7 @@ function mount_sync() {
       # triggers potential network read issue in 9p
       ls $VM_TMP >/dev/null 2>/dev/null
       if [ $? != 0 ]; then
-        echo "remove stale folder"
+        echo "remove stale folder [$VM_TMP]"
         umount $VM_TMP
         rm -d $VM_TMP
       fi
@@ -32,9 +34,12 @@ function mount_sync() {
     if [ ! -e "$VM_TMP" ]; then
       # create mount point
       test -d $HOST_TMP && mkdir $VM_TMP || touch $VM_TMP
+    fi
+
+    if ! mount | grep "$VM_TMP" > /dev/null 2>&1; then
       # bind mount
       mount --bind $HOST_TMP $VM_TMP
-      echo "bind mount $i";
+      echo "bind mount $VM_TMP";
     fi
 
   done
